b956847a9c25ed71a558efbc5f0f658dc3c52924
datikken <tikken23@gmail.com>
2023-02-20 16:25:15 +0300

    orm details

---
+++ orm/main.py
@@ -0,0 +1,23 @@
+from sqlalchemy.orm import Session
+from definitions import engine
+from definitions import User, Address
+
+
+with Session(engine) as session:
+    spongebob = User(
+        name="spongebob",
+        fullname="Spongebob Squarepants",
+        addresses=[Address(email_address="spongebob@sqlalchemy.org")],
+    )
+    sandy = User(
+        name="sandy",
+        fullname="Sandy Cheeks",
+        addresses=[
+            Address(email_address="sandy@sqlalchemy.org"),
+            Address(email_address="sandy@squirrelpower.org"),
+        ],
+    )
+    patrick = User(name="patrick", fullname="Patrick Star")
+    session.add_all([spongebob, sandy, patrick])
+    session.flush()
+    session.commit()
--- unified/shared/engine.py
+++ unified/shared/engine.py
@@ -2,6 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 
-engine = create_engine("sqlite:///database.sqlite", echo=True)
+engine = create_engine("sqlite:///unified/database.sqlite", echo=True)
 
 session = Session(engine)
